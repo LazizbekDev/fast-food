803d44128dc7b7dcc3b3093e1c1d53633cb46162
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-04 12:23:11 +0500

    fetching orders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Cart from "./features/cart/Card"
 import Menu, { loader as menuLoader} from "./features/menu/Menu"
 import CreateOrder from "./features/order/CreateOrder"
-import Order from "./features/order/Order"
+import Order, { loader as orderLoader } from "./features/order/Order"
 import Layout from "./ui/AppLayout"
 import Home from "./ui/Home"
 import Error from "./ui/Error.jsx";
@@ -34,7 +34,9 @@ function App() {
         },
         {
           path: "/order/:orderId",
-          element: <Order />
+          element: <Order />,
+          loader: orderLoader,
+          errorElement: <Error />
         }
       ]
     }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import SearchOrder from "../features/order/SearchOrder.jsx";
 
 const Header = () => {
     return (
         <header>
             <Link to={'/'}>Pizza Co.</Link>
 
+            <SearchOrder />
             <p>content</p>
         </header>
     )
