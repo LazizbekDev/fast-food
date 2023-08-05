0c92744060ffc3c397261ec8bcda8835d357a3e9
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-05 16:38:53 +0500

    font styling

---
+++ src/features/user/username.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Username = () => {
+    return (
+        <div className={'text-sm font-semibold'}>
+            IbnLaziz
+        </div>
+    );
+};
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/username.jsx";
 
 const Header = () => {
     return (
-        <header className={'bg-yellow-500 flex py-2 px-9 justify-between'}>
-            <Link to={'/'}>Pizza Co.</Link>
+        <header className={'bg-yellow-500 flex py-2 px-9 uppercase justify-between'}>
+            <Link to={'/'} className={'tracking-widest'}>Pizza Co.</Link>
 
             <SearchOrder />
-            <p>content</p>
+
+            <Username />
         </header>
     )
 }
