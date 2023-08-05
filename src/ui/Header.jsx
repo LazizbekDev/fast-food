ecca9f33818f927affc7d59d7caff4b4223c7c35
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-05 16:14:18 +0500

    she wanna be billioner so fucking bad

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder.jsx";
 
 const Header = () => {
     return (
-        <header>
+        <header className={'bg-yellow-500 flex py-2 px-9 justify-between'}>
             <Link to={'/'}>Pizza Co.</Link>
 
             <SearchOrder />
