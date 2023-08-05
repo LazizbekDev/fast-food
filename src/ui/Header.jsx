80dcdb5f4afd1baddc5e4904ebeede126631ea8b
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-05 16:49:01 +0500

    spacing

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/username.jsx";
 
 const Header = () => {
     return (
-        <header className={'bg-yellow-500 flex py-2 px-9 uppercase justify-between'}>
+        <header className={'bg-yellow-500 flex py-2 px-9 uppercase justify-between border-b border-stone-200'}>
             <Link to={'/'} className={'tracking-widest'}>Pizza Co.</Link>
 
             <SearchOrder />
