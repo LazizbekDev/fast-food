bfddecc2808335d3c371fc7082ffbcb164690001
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-06 11:25:38 +0500

    ui folder done

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Loader = () => <div className={'loader'} />
+const Loader = () => <div className={'absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm'}>
+    <div className={'loader'} />
+</div>
 
 export default Loader;
